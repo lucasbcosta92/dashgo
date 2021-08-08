bd95921dabb43e78faa34fc33e4d7725b4e89c1a
lucas.costa <lucas.costa@acct.global>
2021-08-08 15:14:05 -0300

    feat: added responsivity in header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,19 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
+interface ProfileProps {
+  showProfileData?: boolean;
+}
 
-export function Profile() {
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Lucas Barbosa</Text>
-        <Text color="gray.300" fontSize="small">
-          email@email.com.br
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Lucas Barbosa</Text>
+          <Text color="gray.300" fontSize="small">
+            email@email.com.br
+          </Text>
+        </Box>
+      )}
 
       <Avatar
         size="md"
