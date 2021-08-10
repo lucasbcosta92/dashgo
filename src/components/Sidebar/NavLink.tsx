a6cb53979a21d5aca139852d1015b1103a6e0c88
lucas.costa <lucas.costa@acct.global>
2021-08-09 21:00:53 -0300

    feat(sidebar): added styles in active links

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -5,7 +5,7 @@ import {
   LinkProps as LinkPropsChakra,
 } from "@chakra-ui/react";
 import { ElementType } from "react";
-import Link from "next/link";
+import { ActiveLink } from "../ActiveLink";
 
 interface NavLinkProps extends LinkPropsChakra {
   icon: ElementType;
@@ -15,13 +15,13 @@ interface NavLinkProps extends LinkPropsChakra {
 
 export function NavLink({ icon, children, href, ...rest }: NavLinkProps) {
   return (
-    <Link href={href} passHref>
+    <ActiveLink href={href} passHref>
       <ChakraLink display="flex" align="center" {...rest}>
         <Icon as={icon} fontSize="20" />
         <Text ml="4" fontWeight="medium">
           {children}
         </Text>
       </ChakraLink>
-    </Link>
+    </ActiveLink>
   );
 }
