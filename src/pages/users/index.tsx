3a419e082175aef655ac96474de39ad9f3382609
lucas.costa <lucas.costa@acct.global>
2021-08-13 18:39:27 -0300

    feat(pagination): added rules for pagination buttons

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -124,7 +124,11 @@ export default function UsersList() {
                 </Tbody>
               </Table>
 
-              <Pagination />
+              <Pagination
+                totalCountOfRegisters={200}
+                currentPage={5}
+                onPageChange={() => {}}
+              />
             </>
           )}
         </Box>
